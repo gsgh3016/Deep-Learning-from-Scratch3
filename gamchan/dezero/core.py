341c67df2aa2475362414ed8d958a743ddddc0e3
Gamchan Kang <gsgh3016@gmail.com>
2023-05-15 17:39:46 +0900

    step 54 done 23/05/15

--- gamchan/dezero/core.py
+++ gamchan/dezero/core.py
@@ -3,11 +3,13 @@ import numpy as np
 import contextlib
 import dezero
 
-# ================================================================================
-# config
-# ================================================================================
+
+# =============================================================================
+# Config
+# =============================================================================
 class Config:
     enable_backprop = True
+    train = True
 
 
 @contextlib.contextmanager
@@ -18,66 +20,126 @@ def using_config(name, value):
         yield
     finally:
         setattr(Config, name, old_value)
-        
+
+
 def no_grad():
     return using_config('enable_backprop', False)
 
-# ================================================================================
-# variable
-# ================================================================================
+
+def test_mode():
+    return using_config('train', False)
+
+# =============================================================================
+# Variable / Function
+# =============================================================================
+try:
+    import cupy
+    array_types = (np.ndarray, cupy.ndarray)
+except ImportError:
+    array_types = (np.ndarray)
+
+
 class Variable:
     __array_priority__ = 200
-    
+
     def __init__(self, data, name=None):
         if data is not None:
-            if not isinstance(data, np.ndarray):
-                raise TypeError('{} is not supported.'.format(type(data)))
-            
+            if not isinstance(data, array_types):
+                raise TypeError('{} is not supported'.format(type(data)))
+
         self.data = data
         self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
-        
-    @property                
+
+    @property
     def shape(self):
         return self.data.shape
-    
+
     @property
     def ndim(self):
         return self.data.ndim
-    
+
     @property
     def size(self):
         return self.data.size
-    
+
     @property
     def dtype(self):
         return self.data.dtype
-    
-    @property
-    def T(self):
-        return dezero.functions.transpose(self)
-    
+
     def __len__(self):
         return len(self.data)
-    
+
     def __repr__(self):
         if self.data is None:
             return 'variable(None)'
-        p = str(self.data).replace('\n', '\n' + ' '*9)
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
         return 'variable(' + p + ')'
-    
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def unchain(self):
+        self.creator = None
+
+    def cleargrad(self):
+        self.grad = None
+
+    def backward(self, retain_grad=False, create_graph=False):
+        if self.grad is None:
+            xp = dezero.cuda.get_array_module(self.data)
+            self.grad = Variable(xp.ones_like(self.data))
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]  # output is weakref
+
+            with using_config('enable_backprop', create_graph):
+                gxs = f.backward(*gys)
+                if not isinstance(gxs, tuple):
+                    gxs = (gxs,)
+
+                for x, gx in zip(f.inputs, gxs):
+                    if x.grad is None:
+                        x.grad = gx
+                    else:
+                        x.grad = x.grad + gx
+
+                    if x.creator is not None:
+                        add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None  # y is weakref
+
+    def unchain_backward(self):
+        if self.creator is not None:
+            funcs = [self.creator]
+            while funcs:
+                f = funcs.pop()
+                for x in f.inputs:
+                    if x.creator is not None:
+                        funcs.append(x.creator)
+                        x.unchain()
+
     def reshape(self, *shape):
         if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
             shape = shape[0]
         return dezero.functions.reshape(self, shape)
-        # not return F.reshape(self, shape)
-        # to avoid circular import
-        
-    def sum(self, axis=None, keepdims=False):
-        return dezero.functions.sum(self, axis, keepdims)
-        
+
     def transpose(self, *axes):
         if len(axes) == 0:
             axes = None
@@ -85,50 +147,25 @@ class Variable:
             if isinstance(axes[0], (tuple, list)) or axes[0] is None:
                 axes = axes[0]
         return dezero.functions.transpose(self, axes)
-        
-    def cleargrad(self):
-        self.grad = None
-        
-    def set_creator(self, func):
-        self.creator = func
-        self.generation = func.generation + 1
-        
-    def backward(self, retain_grad=False, create_graph=False):
-        if self.grad is None:
-            self.grad = Variable(np.ones_like(self.data))
-            
-        funcs = []
-        seen_set = set()
-        
-        def add_func(f):
-            if f not in seen_set:
-                funcs.append(f)
-                seen_set.add(f)
-                funcs.sort(key=lambda x: x.generation)
-                
-        add_func(self.creator)
-        
-        while funcs:
-            f = funcs.pop()
-            gys = [output().grad for output in f.outputs]
-            
-            with using_config('enable_backprop', create_graph):
-                gxs = f.backward(*gys)
-                if not isinstance(gxs, tuple):
-                    gxs = (gxs, )
-                    
-                for x, gx in zip(f.inputs, gxs):
-                    if x.grad is None:
-                        x.grad = gx
-                    else:
-                        x.grad = x.grad + gx
-                    
-                    if x.creator is not None:
-                        add_func(x.creator)
-                    
-            if not retain_grad:
-                for y in f.outputs:
-                    y().grad = None
+
+    @property
+    def T(self):
+        return dezero.functions.transpose(self)
+
+    def sum(self, axis=None, keepdims=False):
+        return dezero.functions.sum(self, axis, keepdims)
+
+    def to_cpu(self):
+        if self.data is not None:
+            self.data = dezero.cuda.as_numpy(self.data)
+
+    def to_gpu(self):
+        if self.data is not None:
+            self.data = dezero.cuda.as_cupy(self.data)
+
+
+class Parameter(Variable):
+    pass
 
 
 def as_variable(obj):
@@ -137,45 +174,41 @@ def as_variable(obj):
     return Variable(obj)
 
 
-def as_array(x):
+def as_array(x, array_module=np):
     if np.isscalar(x):
-        return np.array(x)
+        return array_module.array(x)
     return x
 
-# ================================================================================
-# function
-# ================================================================================                    
+
 class Function:
     def __call__(self, *inputs):
         inputs = [as_variable(x) for x in inputs]
-        
-        # forward propagation(main process)
+
         xs = [x.data for x in inputs]
         ys = self.forward(*xs)
         if not isinstance(ys, tuple):
-            ys = (ys, )
+            ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
-        
+
         if Config.enable_backprop:
             self.generation = max([x.generation for x in inputs])
-            # create relationship between Function and Variable
             for output in outputs:
                 output.set_creator(self)
             self.inputs = inputs
             self.outputs = [weakref.ref(output) for output in outputs]
-        
+
         return outputs if len(outputs) > 1 else outputs[0]
-    
+
     def forward(self, xs):
         raise NotImplementedError()
-    
+
     def backward(self, gys):
         raise NotImplementedError()
 
-# =============================================================================
-# Arithmatic Operation / Operation Overload
-# =============================================================================
 
+# =============================================================================
+# 사칙연산 / 연산자 오버로드
+# =============================================================================
 class Add(Function):
     def forward(self, x0, x1):
         self.x0_shape, self.x1_shape = x0.shape, x1.shape
@@ -188,11 +221,11 @@ class Add(Function):
             gx0 = dezero.functions.sum_to(gx0, self.x0_shape)
             gx1 = dezero.functions.sum_to(gx1, self.x1_shape)
         return gx0, gx1
-    
-def add(x0, x1):
-    x1 = as_array(x1)
-    return Add()(x0, x1)
 
+
+def add(x0, x1):
+    x1 = as_array(x1, dezero.cuda.get_array_module(x0.data))
+    return Add()(x0, x1)
 
 
 class Mul(Function):
@@ -208,23 +241,23 @@ class Mul(Function):
             gx0 = dezero.functions.sum_to(gx0, x0.shape)
             gx1 = dezero.functions.sum_to(gx1, x1.shape)
         return gx0, gx1
-    
-def mul(x0, x1):
-    x1 = as_array(x1)
-    return Mul()(x0, x1)    
 
+
+def mul(x0, x1):
+    x1 = as_array(x1, dezero.cuda.get_array_module(x0.data))
+    return Mul()(x0, x1)
 
 
 class Neg(Function):
     def forward(self, x):
         return -x
-    
+
     def backward(self, gy):
         return -gy
-    
+
+
 def neg(x):
     return Neg()(x)
-    
 
 
 class Sub(Function):
@@ -240,15 +273,16 @@ class Sub(Function):
             gx0 = dezero.functions.sum_to(gx0, self.x0_shape)
             gx1 = dezero.functions.sum_to(gx1, self.x1_shape)
         return gx0, gx1
-    
+
+
 def sub(x0, x1):
-    x1 = as_array(x1)
+    x1 = as_array(x1, dezero.cuda.get_array_module(x0.data))
     return Sub()(x0, x1)
 
+
 def rsub(x0, x1):
-    x1 = as_array(x1)
-    return Sub()(x1, x0)    
-    
+    x1 = as_array(x1, dezero.cuda.get_array_module(x0.data))
+    return Sub()(x1, x0)
 
 
 class Div(Function):
@@ -265,38 +299,36 @@ class Div(Function):
             gx1 = dezero.functions.sum_to(gx1, x1.shape)
         return gx0, gx1
 
-    
+
 def div(x0, x1):
-    x1 = as_array(x1)
+    x1 = as_array(x1, dezero.cuda.get_array_module(x0.data))
     return Div()(x0, x1)
 
+
 def rdiv(x0, x1):
-    x1 = as_array(x1)
+    x1 = as_array(x1, dezero.cuda.get_array_module(x0.data))
     return Div()(x1, x0)
-    
 
 
 class Pow(Function):
     def __init__(self, c):
         self.c = c
-        
+
     def forward(self, x):
         y = x ** self.c
         return y
-    
+
     def backward(self, gy):
         x, = self.inputs
         c = self.c
         gx = c * x ** (c - 1) * gy
         return gx
-    
+
+
 def pow(x, c):
     return Pow(c)(x)
 
 
-# ================================================================================
-# operation overloading
-# ================================================================================                    
 def setup_variable():
     Variable.__add__ = add
     Variable.__radd__ = add
@@ -308,5 +340,9 @@ def setup_variable():
     Variable.__truediv__ = div
     Variable.__rtruediv__ = rdiv
     Variable.__pow__ = pow
-    
-    Variable.matmul = dezero.functions.matmul
+    Variable.__getitem__ = dezero.functions.get_item
+
+    Variable.matmaul = dezero.functions.matmul
+    Variable.dot = dezero.functions.matmul
+    Variable.max = dezero.functions.max
+    Variable.min = dezero.functions.min
---
+++ gamchan/part_4/step50.py
@@ -0,0 +1,92 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import dezero
+from dezero import optimizers, DataLoader
+import dezero.functions as F
+from dezero.models import MLP
+
+# Hyperparameters
+max_epoch = 300
+batch_size = 30
+hidden_size = 10
+lr = 1.0
+
+train_set = dezero.datasets.Spiral(train=True)
+test_set = dezero.datasets.Spiral(train=False)
+train_loader = DataLoader(train_set, batch_size)
+test_loader = DataLoader(test_set, batch_size, shuffle=False)
+
+model = MLP((hidden_size, 3))
+optimizer = optimizers.SGD(lr).setup(model)
+train_loss_list = []
+train_acc_list = []
+test_loss_list = []
+test_acc_list = []
+for epoch in range(max_epoch):
+    sum_loss, sum_acc = 0, 0
+
+    for x, t in train_loader:
+        y = model(x)
+        loss = F.softmax_cross_entropy(y, t)
+        acc = F.accuracy(y, t)
+        model.cleargrads()
+        loss.backward()
+        optimizer.update()
+
+        sum_loss += float(loss.data) * len(t)
+        sum_acc += float(acc.data) * len(t)
+
+    print('epoch {}'.format(epoch + 1))
+    print('train loss: {:.4f}, accuracy: {:.4f}'.format(sum_loss / len(train_set), sum_acc / len(train_set)))
+    train_loss_list.append(sum_loss / len(train_set))
+    train_acc_list.append(sum_acc / len(train_set))
+    
+    sum_loss, sum_acc = 0, 0
+    with dezero.no_grad():
+        for x, t in test_loader:
+            y = model(x)
+            loss = F.softmax_cross_entropy(y, t)
+            acc = F.accuracy(y, t)
+            sum_loss += float(loss.data) * len(t)
+            sum_acc += float(acc.data) * len(t)
+            
+    print('test loss: {:.4f}, accuracy: {:.4f}'.format(sum_loss / len(test_set), sum_acc / len(test_set)))
+    test_loss_list.append(sum_loss / len(test_set))
+    test_acc_list.append(sum_acc / len(test_set))
+
+fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+
+x_axis = np.arange(0, max_epoch, 1)
+# Line graph for y1
+axs[0].plot(x_axis, train_loss_list, label='train')
+
+# Line graph for y2
+axs[0].plot(x_axis, test_loss_list, label='test')
+
+# Set title and labels
+axs[0].set_title('loss')
+axs[0].set_xlabel('epoch')
+axs[0].set_ylabel('loss')
+
+# Add a legend
+axs[0].legend()
+
+axs[1].plot(x_axis, train_acc_list, label='train')
+
+# Line graph for y2
+axs[1].plot(x_axis, test_acc_list, label='test')
+
+# Set title and labels
+axs[1].set_title('acc')
+axs[1].set_xlabel('epoch')
+axs[1].set_ylabel('acc')
+
+# Add a legend
+axs[1].legend()
+
+# Display the figure
+plt.show()
